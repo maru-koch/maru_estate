1f93bf99083f5de013ebd63e43d5e03eff3312cc
maru-koch <nwokochafranklyn@gmail.com>
2022-05-15 00:07:47 +0100

    model setup
    create a custom user and a user account manager

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, name, password = None):
+        if email is None:
+            raise ValueError('email cannot be None')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name = name)
+        user.set_password(password)
+        
+        #: self._db : important on scale beyond one database
+        user.save(using = self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserAccountManager()
+
+    def get_full_name(self):
+        return self.name
+    
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
