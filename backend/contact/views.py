ed644d444b07d5ef0a3040280ec641b58b661621
maru-koch <nwokochafranklyn@gmail.com>
2022-05-17 01:44:42 +0100

    feature - contact: created the ContactCreateView

--- backend/contact/views.py
+++ backend/contact/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from .serializers import ContactSerializer
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Contact
 
-# Create your views here.
+class ContactCreateView(APIView):
+    serializer_class = ContactSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format = None):
+        
+        try:
+            data = self.request.data
+            subject = data['subject']
+            message = 'dear ' + data['name'] +'\n' + data['message']
+            email = data['email']
+            send_mail(subject, message, 'marukoch.ng', [email], fail_silently=  False)
+            contact = Contact(name = data['name'], subject = subject, email = email, message = data['message'])
+            contact.save()
+            return Response({'success':'message sent successfully'})
+        except:
+            return Response({'error': 'message not sent'})
+
