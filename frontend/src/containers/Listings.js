f2cbf1d85bf8a54a8b0e116f7e0dff93c6f25abd
maru-koch <nwokochafranklyn@gmail.com>
2022-05-18 16:31:51 +0100

    feature - listing
    added a funtion to get the listing

--- frontend/src/containers/Listings.js
+++ frontend/src/containers/Listings.js
@@ -1,8 +1,19 @@
+import { useState } from 'react'
+import { listing } from '../services/api'
 
 const Listing =()=>{
+    const [listings, getListings] = useState([])
+
+    const getListing =()=>{
+        const properties = listing()
+        getListings(properties)
+        console.log(listings)
+    }
+
     return (
     <div>
         <h1>Property Listing</h1>
+        <button onClick ={getListing}></button>
     </div>
     )
 }
