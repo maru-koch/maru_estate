9c5d01f4e48199bbdc1da548b1d16347ef65b14a
maru-koch <nwokochafranklyn@gmail.com>
2022-05-19 19:57:12 +0100

    feature - contact
    implemented sending email

--- backend/contact/views.py
+++ backend/contact/views.py
@@ -1,4 +1,4 @@
-from django.core.mail import send_mail
+from django.core.mail import send_mail, BadHeaderError
 from .serializers import ContactSerializer
 from rest_framework import permissions
 from rest_framework.views import APIView
@@ -12,15 +12,19 @@ class ContactCreateView(APIView):
 
     def post(self, request, format = None):
         
-        try:
-            data = self.request.data
-            subject = data['subject']
-            message = 'dear ' + data['name'] +'\n' + data['message']
-            email = data['email']
-            send_mail(subject, message, 'marukoch.ng', [email], fail_silently=  False)
-            contact = Contact(name = data['name'], subject = subject, email = email, message = data['message'])
-            contact.save()
-            return Response({'success':'message sent successfully'})
-        except:
-            return Response({'error': 'message not sent'})
+        data = self.request.data
+        subject = data['subject']
+        message = '\ndear ' + data['name'] +'\n' + data['message']
+        email = data['email']
+        name = data['name']
 
+        if subject and message and email and name:
+            try:
+                send_mail(subject, message, 'marukoch.ng', [email], fail_silently=  False)
+                contact = Contact(name = name, subject = subject, email = email, message = message)
+                contact.save()
+                return Response({'success':'message sent successfully'})
+            except BadHeaderError:
+                return Response({'error': 'Your email is compromised!'})
+        else:
+            return Response({'error': 'Invalid email credentials'})
